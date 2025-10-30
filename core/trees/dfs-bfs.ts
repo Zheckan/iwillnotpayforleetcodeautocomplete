2023a07893e936794c2779c48023e2a319f7b143
Yevhenii Zhaivoronok <Zheckan1120@gmail.com>
2025-10-30 12:27:07 +0100

    Add arrays-strings.ts and modify dfs-bfs.ts

--- core/trees/dfs-bfs.ts
+++ core/trees/dfs-bfs.ts
@@ -1,3 +1,14 @@
+/*
+      1
+    / \
+    2   3
+    / \
+   4   5
+
+Preowrder:  1 → 2 → 4 → 5 → 3
+Inorder:   4 → 2 → 5 → 1 → 3
+Postorder: 4 → 5 → 2 → 3 → 1
+*/
 class Tree {
   current: number;
   left: Tree | null;
